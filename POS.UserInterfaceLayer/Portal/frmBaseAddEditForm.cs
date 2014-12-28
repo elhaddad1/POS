e4bc225be91258835e432bad72ed918e883c5608
mohamed-khalil88 <eng.mohamed_khalil88@yahoo.com>
2014-12-28 20:52:40 +0200

    zzzzzzzzzzzz

---
+++ POS.UserInterfaceLayer/Portal/frmBaseAddEditForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POS.UserInterfaceLayer.Portal
+{
+    public partial class frmBaseAddEditForm : Form
+    {
+        public frmBaseAddEditForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Events implementation
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+        public virtual void btn_Save_Click(object sender, EventArgs e) { }
+
+        public virtual void btn_Back_Click(object sender, EventArgs e) { }
+
+    }
+}
