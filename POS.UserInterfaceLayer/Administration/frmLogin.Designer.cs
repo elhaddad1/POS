897accd3706e069d97c27b1e3bc1da2d22e58315
mohamed-khalil88 <eng.mohamed_khalil88@yahoo.com>
2014-12-30 23:39:00 +0200

    Creating login screen

--- POS.UserInterfaceLayer/Administration/frmLogin.Designer.cs
+++ POS.UserInterfaceLayer/Administration/frmLogin.Designer.cs
@@ -28,20 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lbl_UserName = new System.Windows.Forms.Label();
+            this.lbl_Password = new System.Windows.Forms.Label();
+            this.tbx_UserName = new System.Windows.Forms.TextBox();
+            this.tbx_Password = new System.Windows.Forms.TextBox();
+            this.btn_Enter = new System.Windows.Forms.Button();
+            this.btn_Back = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // lbl_UserName
+            // 
+            this.lbl_UserName.AutoSize = true;
+            this.lbl_UserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_UserName.Location = new System.Drawing.Point(43, 63);
+            this.lbl_UserName.Name = "lbl_UserName";
+            this.lbl_UserName.Size = new System.Drawing.Size(66, 20);
+            this.lbl_UserName.TabIndex = 0;
+            this.lbl_UserName.Text = "أسم الدخول";
+            // 
+            // lbl_Password
+            // 
+            this.lbl_Password.AutoSize = true;
+            this.lbl_Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Password.Location = new System.Drawing.Point(43, 114);
+            this.lbl_Password.Name = "lbl_Password";
+            this.lbl_Password.Size = new System.Drawing.Size(69, 20);
+            this.lbl_Password.TabIndex = 2;
+            this.lbl_Password.Text = "رمز الدخول";
+            // 
+            // tbx_UserName
+            // 
+            this.tbx_UserName.Location = new System.Drawing.Point(135, 63);
+            this.tbx_UserName.Name = "tbx_UserName";
+            this.tbx_UserName.Size = new System.Drawing.Size(255, 20);
+            this.tbx_UserName.TabIndex = 3;
+            // 
+            // tbx_Password
+            // 
+            this.tbx_Password.Location = new System.Drawing.Point(135, 114);
+            this.tbx_Password.Name = "tbx_Password";
+            this.tbx_Password.PasswordChar = '*';
+            this.tbx_Password.Size = new System.Drawing.Size(255, 20);
+            this.tbx_Password.TabIndex = 4;
+            // 
+            // btn_Enter
+            // 
+            this.btn_Enter.Location = new System.Drawing.Point(117, 160);
+            this.btn_Enter.Name = "btn_Enter";
+            this.btn_Enter.Size = new System.Drawing.Size(87, 50);
+            this.btn_Enter.TabIndex = 5;
+            this.btn_Enter.Text = "دخول";
+            this.btn_Enter.UseVisualStyleBackColor = true;
+            // 
+            // btn_Back
+            // 
+            this.btn_Back.Location = new System.Drawing.Point(228, 160);
+            this.btn_Back.Name = "btn_Back";
+            this.btn_Back.Size = new System.Drawing.Size(87, 50);
+            this.btn_Back.TabIndex = 6;
+            this.btn_Back.Text = "رجوع";
+            this.btn_Back.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(72, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(288, 50);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
             // 
             // frmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(433, 222);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_Back);
+            this.Controls.Add(this.btn_Enter);
+            this.Controls.Add(this.tbx_Password);
+            this.Controls.Add(this.tbx_UserName);
+            this.Controls.Add(this.lbl_Password);
+            this.Controls.Add(this.lbl_UserName);
             this.Name = "frmLogin";
-            this.Text = "frmLogin";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "دخول";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl_UserName;
+        private System.Windows.Forms.Label lbl_Password;
+        private System.Windows.Forms.TextBox tbx_UserName;
+        private System.Windows.Forms.TextBox tbx_Password;
+        private System.Windows.Forms.Button btn_Enter;
+        private System.Windows.Forms.Button btn_Back;
+        private System.Windows.Forms.PictureBox pictureBox1;
 
     }
 }
