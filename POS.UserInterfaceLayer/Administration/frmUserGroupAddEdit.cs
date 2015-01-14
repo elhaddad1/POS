41f679089441eca5f3ae0b9a489783477a9e5965
mohamed-khalil88 <eng.mohamed_khalil88@yahoo.com>
2015-01-14 22:51:48 +0200

    iii

--- POS.UserInterfaceLayer/Administration/frmUserGroupAddEdit.cs
+++ POS.UserInterfaceLayer/Administration/frmUserGroupAddEdit.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using POS.BusinessLayer;
 using POS.BusinessLayer.Wrapper;
@@ -14,12 +16,17 @@ namespace POS.UserInterfaceLayer.Administration
     public partial class frmUserGroupAddEdit : POS.UserInterfaceLayer.Portal.frmBaseAddEditForm
     {
         ADGroupWrapper _aDGroupWrapper;
+        ADRoleWrapper _aDRoleWrapper;
+        ADGroupRoleWrapper _aDGroupRoleWrapper;
         private int _groupID = 0;
+
         public frmUserGroupAddEdit()
         {
             InitializeComponent();
             base.lbl_FormHeader.Text = "أضافة مجموعة";
             _aDGroupWrapper = new ADGroupWrapper();
+            _aDRoleWrapper = new ADRoleWrapper();
+            FillRolesDataGrid();
         }
 
         public frmUserGroupAddEdit(int groupID)
@@ -27,7 +34,11 @@ namespace POS.UserInterfaceLayer.Administration
             InitializeComponent();
             base.lbl_FormHeader.Text = "تعديل مجموعة";
             _aDGroupWrapper = new ADGroupWrapper();
+            _aDRoleWrapper = new ADRoleWrapper();
+            _aDGroupRoleWrapper = new ADGroupRoleWrapper();
+
             _groupID = groupID;
+            FillRolesDataGrid();
             InitiateEntity(groupID);
         }
 
@@ -70,12 +81,43 @@ namespace POS.UserInterfaceLayer.Administration
         /// Private methods
         /// </summary>
         /// <param name="groupID"></param>
+        /// 
+        private void FillRolesDataGrid()
+        {
+            dgrid_Roles.DataSource = null;
+            dgrid_Roles.AutoGenerateColumns = false;
+
+            dgrid_Roles.DataSource = _aDRoleWrapper.SelectAll();
+            dgrid_Roles.Columns[0].DataPropertyName = "RoleID";
+            dgrid_Roles.Columns[2].DataPropertyName = "RoleName";
+            for (int i = 0; i < dgrid_Roles.Rows.Count; i++)
+            {
+                DataGridViewCheckBoxCell chk = (DataGridViewCheckBoxCell)dgrid_Roles.Rows[i].Cells["check"];
+                chk.Selected = false;
+                chk.Value = 1;
+            }
+
+        }
         private void InitiateEntity(int groupID)
         {
             ADGroupPrimaryKey pk = new ADGroupPrimaryKey();
             pk.GroupID = groupID;
             ADGroup _aDGroup = _aDGroupWrapper.SelectOne(pk);
             tbx_GroupName.Text = _aDGroup.GroupName;
+
+            ADGroupRoleCollection _aDGroupRoleCollection = _aDGroupRoleWrapper.SelectByGroupID(groupID);
+            foreach (ADGroupRole _aDGroupRole in _aDGroupRoleCollection)
+            {
+                for (int i = 0; i < dgrid_Roles.Rows.Count; i++)
+                {
+                    if (Convert.ToInt32(dgrid_Roles.Rows[i].Cells["RoleID"].Value) == _aDGroupRole.RoleID)
+                    {
+                        DataGridViewCheckBoxCell chk = (DataGridViewCheckBoxCell)dgrid_Roles.Rows[i].Cells["check"];
+                        chk.Selected = true;
+                        chk.Value = 1;
+                    }
+                }
+            }
         }
         private bool Valdation()
         {
