c0b0c2d5d43e70eeeb8045d87ffa6fbc582ef3a7
elhaddad1 <a.s.elhaddad@gmail.com>
2015-03-14 22:36:25 +0200

    adjust stock

--- POS.UserInterfaceLayer/BasicData/FrmAdjustStockAddEdit.cs
+++ POS.UserInterfaceLayer/BasicData/FrmAdjustStockAddEdit.cs
@@ -1,18 +1,36 @@
-﻿using System;
+﻿using POS.BusinessLayer;
+using POS.UserInterfaceLayer.Portal;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace POS.UserInterfaceLayer.BasicData
 {
     public partial class FrmAdjustStockAddEdit : POS.UserInterfaceLayer.Portal.frmBaseAddEditForm
     {
-        public FrmAdjustStockAddEdit()
+        INVAdjustStockReason _currentReason;
+        INVAdjustStockReasonService service=new INVAdjustStockReasonService ();
+        public FrmAdjustStockAddEdit(FrmAdjustmentSearch frmAdjustmentSearch)
         {
             InitializeComponent();
+        }
+        public FrmAdjustStockAddEdit(int id, FrmAdjustmentSearch frmAdjustmentSearch)
+        {
+            InitializeComponent();
+
+        }
+        public override void btn_Save_Click(object sender, EventArgs e)
+        {
+        }
+        public override void btn_Back_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
