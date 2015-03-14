c1bdc3732ddc472e4cd52b7ec79b27088707b1de
elhaddad1 <a.s.elhaddad@gmail.com>
2015-03-14 22:06:40 +0200

    adjust stock add edit

---
+++ POS.UserInterfaceLayer/BasicData/FrmAdjustStockAddEdit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace POS.UserInterfaceLayer.BasicData
+{
+    public partial class FrmAdjustStockAddEdit : POS.UserInterfaceLayer.Portal.frmBaseAddEditForm
+    {
+        public FrmAdjustStockAddEdit()
+        {
+            InitializeComponent();
+        }
+    }
+}
