266c04c30ca7792d1c23daaeb6e8459e3efc2763
elhaddad1 <a.s.elhaddad@gmail.com>
2015-03-17 00:50:06 +0200

    adjust stock

--- POS.UserInterfaceLayer/BasicData/FrmAdjustStockAddEdit.Designer.cs
+++ POS.UserInterfaceLayer/BasicData/FrmAdjustStockAddEdit.Designer.cs
@@ -30,53 +30,57 @@
         {
             this.cbx_StockTypeTO = new System.Windows.Forms.ComboBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.cbx_StockTypeFrom = new System.Windows.Forms.ComboBox();
-            this.label5 = new System.Windows.Forms.Label();
             this.num_Quantity = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.cbx_AdjustReason = new System.Windows.Forms.ComboBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.BatchNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.ExpiryDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.cbx_Product = new System.Windows.Forms.ComboBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.dgrid_batches = new System.Windows.Forms.DataGridView();
+            this.col_batch = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col_expiry = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col_batchQty = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgrid_stock = new System.Windows.Forms.DataGridView();
+            this.col_code = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col_invProductStock = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col_stockType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col_qty = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.cbx_Store = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.panl_Header.SuspendLayout();
             this.panl_Footer.SuspendLayout();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgrid_batches)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgrid_stock)).BeginInit();
             this.SuspendLayout();
             // 
             // panl_Header
             // 
-            this.panl_Header.Size = new System.Drawing.Size(983, 53);
+            this.panl_Header.Size = new System.Drawing.Size(962, 53);
             // 
             // lbl_FormHeader
             // 
-            this.lbl_FormHeader.Location = new System.Drawing.Point(476, 21);
+            this.lbl_FormHeader.Location = new System.Drawing.Point(1729, 21);
             // 
             // panl_Footer
             // 
-            this.panl_Footer.Location = new System.Drawing.Point(0, 280);
-            this.panl_Footer.Size = new System.Drawing.Size(983, 62);
+            this.panl_Footer.Location = new System.Drawing.Point(0, 413);
+            this.panl_Footer.Size = new System.Drawing.Size(962, 62);
             // 
             // btn_Save
             // 
-            this.btn_Save.Location = new System.Drawing.Point(476, 7);
+            this.btn_Save.Location = new System.Drawing.Point(1729, 7);
             // 
             // btn_Back
             // 
-            this.btn_Back.Location = new System.Drawing.Point(373, 7);
+            this.btn_Back.Location = new System.Drawing.Point(1188, 7);
             // 
             // cbx_StockTypeTO
             // 
             this.cbx_StockTypeTO.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
             this.cbx_StockTypeTO.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
             this.cbx_StockTypeTO.FormattingEnabled = true;
-            this.cbx_StockTypeTO.Location = new System.Drawing.Point(489, 242);
+            this.cbx_StockTypeTO.Location = new System.Drawing.Point(135, 222);
             this.cbx_StockTypeTO.Name = "cbx_StockTypeTO";
             this.cbx_StockTypeTO.Size = new System.Drawing.Size(241, 24);
             this.cbx_StockTypeTO.TabIndex = 76;
@@ -84,34 +88,15 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(386, 245);
+            this.label6.Location = new System.Drawing.Point(14, 222);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(84, 16);
             this.label6.TabIndex = 75;
             this.label6.Text = "نوع الصنف - الى";
             // 
-            // cbx_StockTypeFrom
-            // 
-            this.cbx_StockTypeFrom.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
-            this.cbx_StockTypeFrom.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
-            this.cbx_StockTypeFrom.FormattingEnabled = true;
-            this.cbx_StockTypeFrom.Location = new System.Drawing.Point(133, 242);
-            this.cbx_StockTypeFrom.Name = "cbx_StockTypeFrom";
-            this.cbx_StockTypeFrom.Size = new System.Drawing.Size(241, 24);
-            this.cbx_StockTypeFrom.TabIndex = 74;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(30, 245);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(83, 16);
-            this.label5.TabIndex = 73;
-            this.label5.Text = "نوع الصنف - من";
-            // 
             // num_Quantity
             // 
-            this.num_Quantity.Location = new System.Drawing.Point(133, 198);
+            this.num_Quantity.Location = new System.Drawing.Point(135, 186);
             this.num_Quantity.Name = "num_Quantity";
             this.num_Quantity.Size = new System.Drawing.Size(241, 22);
             this.num_Quantity.TabIndex = 72;
@@ -120,7 +105,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(32, 203);
+            this.label4.Location = new System.Drawing.Point(14, 192);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(33, 16);
             this.label4.TabIndex = 71;
@@ -131,73 +116,131 @@
             this.cbx_AdjustReason.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
             this.cbx_AdjustReason.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
             this.cbx_AdjustReason.FormattingEnabled = true;
-            this.cbx_AdjustReason.Location = new System.Drawing.Point(133, 120);
+            this.cbx_AdjustReason.Location = new System.Drawing.Point(135, 146);
             this.cbx_AdjustReason.Name = "cbx_AdjustReason";
             this.cbx_AdjustReason.Size = new System.Drawing.Size(241, 24);
             this.cbx_AdjustReason.TabIndex = 70;
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.dataGridView1);
-            this.groupBox1.Location = new System.Drawing.Point(592, 63);
+            this.groupBox1.Controls.Add(this.dgrid_batches);
+            this.groupBox1.Controls.Add(this.dgrid_stock);
+            this.groupBox1.Location = new System.Drawing.Point(391, 59);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(314, 156);
+            this.groupBox1.Size = new System.Drawing.Size(568, 320);
             this.groupBox1.TabIndex = 69;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "الباتشات";
             // 
-            // dataGridView1
+            // dgrid_batches
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.BatchNumber,
-            this.ExpiryDate});
-            this.dataGridView1.Location = new System.Drawing.Point(25, 21);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(264, 123);
-            this.dataGridView1.TabIndex = 54;
+            this.dgrid_batches.AllowUserToAddRows = false;
+            this.dgrid_batches.AllowUserToDeleteRows = false;
+            this.dgrid_batches.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgrid_batches.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.col_batch,
+            this.col_expiry,
+            this.col_batchQty});
+            this.dgrid_batches.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgrid_batches.Location = new System.Drawing.Point(3, 193);
+            this.dgrid_batches.Name = "dgrid_batches";
+            this.dgrid_batches.ReadOnly = true;
+            this.dgrid_batches.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.dgrid_batches.Size = new System.Drawing.Size(562, 124);
+            this.dgrid_batches.TabIndex = 5;
             // 
-            // BatchNumber
+            // col_batch
             // 
-            this.BatchNumber.HeaderText = "رقم الباتش";
-            this.BatchNumber.Name = "BatchNumber";
+            this.col_batch.DataPropertyName = "BatchNumber";
+            this.col_batch.HeaderText = "رقم التشغيله";
+            this.col_batch.Name = "col_batch";
+            this.col_batch.ReadOnly = true;
             // 
-            // ExpiryDate
+            // col_expiry
             // 
-            this.ExpiryDate.HeaderText = "تاريخ الصلاحيه";
-            this.ExpiryDate.Name = "ExpiryDate";
+            this.col_expiry.DataPropertyName = "ExpiryDate";
+            this.col_expiry.HeaderText = "تاريخ الصلاحيه";
+            this.col_expiry.Name = "col_expiry";
+            this.col_expiry.ReadOnly = true;
             // 
-            // cbx_Product
+            // col_batchQty
             // 
-            this.cbx_Product.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
-            this.cbx_Product.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
-            this.cbx_Product.FormattingEnabled = true;
-            this.cbx_Product.Location = new System.Drawing.Point(133, 157);
-            this.cbx_Product.Name = "cbx_Product";
-            this.cbx_Product.Size = new System.Drawing.Size(241, 24);
-            this.cbx_Product.TabIndex = 68;
+            this.col_batchQty.DataPropertyName = "BatchQty";
+            this.col_batchQty.HeaderText = "الكميه";
+            this.col_batchQty.Name = "col_batchQty";
+            this.col_batchQty.ReadOnly = true;
             // 
-            // label2
+            // dgrid_stock
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(18, 160);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(58, 16);
-            this.label2.TabIndex = 67;
-            this.label2.Text = "أسم الصنف";
+            this.dgrid_stock.AllowUserToAddRows = false;
+            this.dgrid_stock.AllowUserToDeleteRows = false;
+            this.dgrid_stock.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgrid_stock.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.col_code,
+            this.col_invProductStock,
+            this.col_name,
+            this.col_stockType,
+            this.col_qty});
+            this.dgrid_stock.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dgrid_stock.Location = new System.Drawing.Point(3, 18);
+            this.dgrid_stock.Name = "dgrid_stock";
+            this.dgrid_stock.ReadOnly = true;
+            this.dgrid_stock.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.dgrid_stock.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dgrid_stock.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgrid_stock.Size = new System.Drawing.Size(562, 169);
+            this.dgrid_stock.TabIndex = 4;
+            // 
+            // col_code
+            // 
+            this.col_code.DataPropertyName = "ProductCode";
+            this.col_code.HeaderText = "كود المنتج";
+            this.col_code.Name = "col_code";
+            this.col_code.ReadOnly = true;
+            // 
+            // col_invProductStock
+            // 
+            this.col_invProductStock.DataPropertyName = "ProductStockID";
+            this.col_invProductStock.HeaderText = "Column1";
+            this.col_invProductStock.Name = "col_invProductStock";
+            this.col_invProductStock.ReadOnly = true;
+            this.col_invProductStock.Visible = false;
+            // 
+            // col_name
+            // 
+            this.col_name.DataPropertyName = "ProductName";
+            this.col_name.HeaderText = "اسم المنتج";
+            this.col_name.Name = "col_name";
+            this.col_name.ReadOnly = true;
+            this.col_name.Width = 200;
+            // 
+            // col_stockType
+            // 
+            this.col_stockType.DataPropertyName = "StockType";
+            this.col_stockType.HeaderText = "نوع المخزون";
+            this.col_stockType.Name = "col_stockType";
+            this.col_stockType.ReadOnly = true;
+            // 
+            // col_qty
+            // 
+            this.col_qty.DataPropertyName = "TotalQty";
+            this.col_qty.HeaderText = "الكميه";
+            this.col_qty.Name = "col_qty";
+            this.col_qty.ReadOnly = true;
             // 
             // cbx_Store
             // 
             this.cbx_Store.FormattingEnabled = true;
-            this.cbx_Store.Location = new System.Drawing.Point(133, 75);
+            this.cbx_Store.Location = new System.Drawing.Point(135, 101);
             this.cbx_Store.Name = "cbx_Store";
             this.cbx_Store.Size = new System.Drawing.Size(241, 24);
             this.cbx_Store.TabIndex = 66;
+            this.cbx_Store.SelectedIndexChanged += new System.EventHandler(this.cbx_Store_SelectedIndexChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 83);
+            this.label3.Location = new System.Drawing.Point(14, 109);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(91, 16);
             this.label3.TabIndex = 65;
@@ -206,7 +249,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 120);
+            this.label1.Location = new System.Drawing.Point(14, 146);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(65, 16);
             this.label1.TabIndex = 64;
@@ -215,17 +258,13 @@
             // FrmAdjustStockAddEdit
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.ClientSize = new System.Drawing.Size(983, 342);
+            this.ClientSize = new System.Drawing.Size(962, 475);
             this.Controls.Add(this.cbx_StockTypeTO);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.cbx_StockTypeFrom);
-            this.Controls.Add(this.label5);
             this.Controls.Add(this.num_Quantity);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.cbx_AdjustReason);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.cbx_Product);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.cbx_Store);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
@@ -235,21 +274,18 @@
             this.Controls.SetChildIndex(this.label1, 0);
             this.Controls.SetChildIndex(this.label3, 0);
             this.Controls.SetChildIndex(this.cbx_Store, 0);
-            this.Controls.SetChildIndex(this.label2, 0);
-            this.Controls.SetChildIndex(this.cbx_Product, 0);
             this.Controls.SetChildIndex(this.groupBox1, 0);
             this.Controls.SetChildIndex(this.cbx_AdjustReason, 0);
             this.Controls.SetChildIndex(this.label4, 0);
             this.Controls.SetChildIndex(this.num_Quantity, 0);
-            this.Controls.SetChildIndex(this.label5, 0);
-            this.Controls.SetChildIndex(this.cbx_StockTypeFrom, 0);
             this.Controls.SetChildIndex(this.label6, 0);
             this.Controls.SetChildIndex(this.cbx_StockTypeTO, 0);
             this.panl_Header.ResumeLayout(false);
             this.panl_Header.PerformLayout();
             this.panl_Footer.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgrid_batches)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgrid_stock)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -259,19 +295,22 @@
 
         private System.Windows.Forms.ComboBox cbx_StockTypeTO;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.ComboBox cbx_StockTypeFrom;
-        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox num_Quantity;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ComboBox cbx_AdjustReason;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn BatchNumber;
-        private System.Windows.Forms.DataGridViewTextBoxColumn ExpiryDate;
-        private System.Windows.Forms.ComboBox cbx_Product;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox cbx_Store;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgrid_batches;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_batch;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_expiry;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_batchQty;
+        private System.Windows.Forms.DataGridView dgrid_stock;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_code;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_invProductStock;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_stockType;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_qty;
     }
 }
--- POS.UserInterfaceLayer/BasicData/FrmAdjustStockAddEdit.cs
+++ POS.UserInterfaceLayer/BasicData/FrmAdjustStockAddEdit.cs
@@ -1,4 +1,5 @@
 ﻿using POS.BusinessLayer;
+using POS.BusinessLayer.Wrapper;
 using POS.UserInterfaceLayer.Portal;
 using System;
 using System.Collections.Generic;
@@ -14,16 +15,18 @@ namespace POS.UserInterfaceLayer.BasicData
 {
     public partial class FrmAdjustStockAddEdit : POS.UserInterfaceLayer.Portal.frmBaseAddEditForm
     {
-        INVAdjustStockReason _currentReason;
-        INVAdjustStockReasonService service=new INVAdjustStockReasonService ();
+        private INVAdjustStockReason _currentReason;
+        private INVAdjustStockReasonService service=new INVAdjustStockReasonService ();
+        private INVProductStockWrapper _invProductStockWrapper;
         public FrmAdjustStockAddEdit(FrmAdjustmentSearch frmAdjustmentSearch)
         {
             InitializeComponent();
+            this._invProductStockWrapper = new INVProductStockWrapper();
         }
         public FrmAdjustStockAddEdit(int id, FrmAdjustmentSearch frmAdjustmentSearch)
         {
             InitializeComponent();
-
+            this._invProductStockWrapper = new INVProductStockWrapper();
         }
         public override void btn_Save_Click(object sender, EventArgs e)
         {
@@ -31,6 +34,31 @@ namespace POS.UserInterfaceLayer.BasicData
         public override void btn_Back_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void FillProductStock()
+        {
+            dgrid_stock.DataSource = _invProductStockWrapper.SelectByField(Convert.ToInt32(cbx_Store.SelectedValue));
+        }
+
+
+        private void FillBatches()
+        {
+
+            int _productStockID = (int)dgrid_stock.SelectedRows[0].Cells["col_invProductStock"].Value;
+             INVProductStockWrapper _iNVProductStockWrapper = new INVProductStockWrapper();
+            dgrid_batches.AutoGenerateColumns = false;
+            dgrid_batches.DataSource = _iNVProductStockWrapper.GetInventoryStock(_productStockID, null, null, true, true);
+        }
+
+        private void cbx_Store_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbx_Store.SelectedValue == null)
+                return;
+            int stkId = 0;
+            int.TryParse(cbx_Store.SelectedValue.ToString(), out stkId);
+            if (stkId > 0)
+                FillProductStock();
         }
     }
 }
